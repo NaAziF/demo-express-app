a43b353ad011e8b06d6aad3f825c52e8f8ce0b70
Faizan Dar <42547319+faizan1234dar@users.noreply.github.com>
2023-04-13 10:37:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 const express = require('express');
 const app = express(); app.listen(8080);
-app.get('/', (req, res) => res.send("Hello User"));
+app.get('/', (req, res) => res.send("<h1>Hello User This is New Text</h1>"));
