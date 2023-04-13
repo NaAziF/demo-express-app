98de67593c7ceba981cd866a5a3fa560dda821dc
Faizan Dar <42547319+faizan1234dar@users.noreply.github.com>
2023-04-13 10:59:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 const express = require('express');
 const app = express(); app.listen(8080);
-app.get('/', (req, res) => res.send("<h1 style="color:green">Hello User This is New Text</h1>"));
+app.get('/', (req, res) => res.send("<h1 style='color:green'>Hello User This is New Text</h1>"));
